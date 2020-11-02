94aa0fb7d76ee3dd62aac0d0027b7bb2ed29d380
lee1409 <leerun1409@gmail.com>
2020-11-02 08:58:18 +0800

    added emulator:start

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,45 @@
-const { spawn } = require("child_process");
-const path = require('path');
+const { spawn, exec } = require("child_process");
+const path = require("path");
+const { stdout } = require("process");
+const readline = require('readline');
 
-const emulator = spawn(path.join(process.env.ANDROID_SDK_ROOT, "tools/emulator"), ['-list-avds'])
-
-emulator.stdout.on("data", data => {
-  console.log(`stdout: ${data.toString()}`);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
 
-emulator.stderr.on("data", data => {
-  console.log(`stderr: ${data}`);
+if (!process.env.ANDROID_SDK_ROOT) {
+  throw new Error("Please set ANDROID_SDK_ROOT at your env")
+}
+
+const listEmulator = spawn(
+  path.join("emulator"),
+  ["-list-avds"]
+);
+
+let emulators = []
+
+listEmulator.stdout.on("data", (data) => {
+  emulators.push(data.toString().trim())
 });
 
-emulator.on('error', (error) => {
-  console.log(`error: ${error.message}`);
-});
+listEmulator.on("close", () => {
+  for (let i = 0; i < emulators.length; i++) {
+    console.log(`${i}: ${emulators[i]}\n`)
+  }
+  readResponse()
+})
 
-emulator.on("close", code => {
-  console.log(`child process exited with code ${code}`);
-});
+function readResponse() {
+  rl.question("Which emulator you would like to choose? ", (answer) => {
+    exec(`emulator @${emulators[answer]}`, (stdout, error) => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+      }
+      console.log(stdout)
+    })
+    rl.close()
+  })
+}
