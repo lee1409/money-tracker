6a9d1aff852c572fbac2b3572eb2ebbc7fe508ad
lee1409 <leerun1409@gmail.com>
2020-12-24 13:14:06 +0800

    added react navigation

---
+++ src/screens/index.js
@@ -0,0 +1,20 @@
+// In App.js in a new project
+
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { ScreenStack } from "react-native-screens";
+import HomeScreen from "./home-screen";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
