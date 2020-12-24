7e3c10da072c51b56f28a71c5e47f4d3b525a441
lee1409 <leerun1409@gmail.com>
2020-12-24 16:52:38 +0800

    prevent persisting

--- configureStore.js
+++ configureStore.js
@@ -1,20 +1,25 @@
-import { createStore } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import rootReducer from "./src/redux/reducers";
 
 const persistConfig = {
-  key: 'root',
+  version: 1,
+  key: "root",
   storage: AsyncStorage,
-}
+};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default () => {
   let store = createStore(
     persistedReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-  let persistor = persistStore(store)
-  return { store, persistor }
-}
+  );
+  // Prevent persisting when development
+  let persistor =
+    process.env.NODE_ENV !== "production"
+      ? persistStore(store, {manualPersist: true})
+      : persistStore(store);
+  return { store, persistor };
+};
