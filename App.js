c2ad7dcb251bea44f75f170666d7792307e252be
lee1409 <leerun1409@gmail.com>
2020-12-25 19:34:15 +0800

    add global theme

--- App.js
+++ App.js
@@ -1,31 +1,53 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Provider as PaperProvider, ProgressBar } from "react-native-paper";
+import {
+  Provider as PaperProvider,
+  ProgressBar,
+  DefaultTheme,
+} from "react-native-paper";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import configureStore from "./configureStore";
-import Screen from './src/screens/index'
+import Screen from "./src/screens/index";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+var _ = require("lodash");
+
+let myTheme = {
+  myOwnProperty: true,
+  colors: {
+    // Red
+    primary: "#EF7971",
+    // Sand
+    primary2: "#F0CFA3",
+    // Abit milky white
+    primary3: "#FFF9F0",
+    // lively green
+    secondary: "#72C4A6",
+    // white sand
+    accent: "#F9ECC3",
+    // grass
+    accent2: "#488B80",
+    text: "#717171",
+  },
+};
+
+let theme = _.merge(DefaultTheme, myTheme);
 
 const { store, persistor } = configureStore();
 
 export default function App() {
   return (
     <Provider store={store}>
-      {/* <PersistGate loading={<ProgressBar indeterminate />} persistor={persistor}> */}
-        <PaperProvider>
+      <SafeAreaProvider>
+        {/* <PersistGate loading={<ProgressBar indeterminate />} persistor={persistor}> */}
+
+        <PaperProvider theme={theme}>
           <Screen></Screen>
         </PaperProvider>
-      {/* </PersistGate> */}
+        {/* </PersistGate> */}
+      </SafeAreaProvider>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
