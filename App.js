4b23f43d0da94d89d03160471efc8741730ed4ac
lee1409 <leerun1409@gmail.com>
2020-12-25 11:17:50 +0800

    off persist gate for development

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
+import { Provider as PaperProvider, ProgressBar } from "react-native-paper";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import configureStore from "./configureStore";
@@ -12,11 +12,11 @@ const { store, persistor } = configureStore();
 export default function App() {
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
+      {/* <PersistGate loading={<ProgressBar indeterminate />} persistor={persistor}> */}
         <PaperProvider>
           <Screen></Screen>
         </PaperProvider>
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   );
 }
