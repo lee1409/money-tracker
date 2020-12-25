30e57e4b5efc385e88d9b8cc118d19b4b10a31a5
lee1409 <leerun1409@gmail.com>
2020-12-25 19:35:29 +0800

    added styling to create event

--- src/screens/create-event-screen.js
+++ src/screens/create-event-screen.js
@@ -1,6 +1,15 @@
 import React from "react";
-import { View } from "react-native";
-import { TextInput, Text, Menu, Button } from "react-native-paper";
+import { StyleSheet, View, Image, KeyboardAvoidingView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  TextInput,
+  Text,
+  Menu,
+  Button,
+  useTheme,
+  IconButton,
+  TouchableRipple,
+} from "react-native-paper";
 import { addEvent } from "../redux/actions/index";
 import { useDispatch } from "react-redux";
 import { createUUID } from "../utils/index";
@@ -16,6 +25,7 @@ const days = [
 ];
 
 export default function CreateEventScreen({ route, navigation }) {
+  const { colors } = useTheme();
   const [form, setForm] = React.useState({
     uid: createUUID(),
     amount: null,
@@ -51,47 +61,148 @@ export default function CreateEventScreen({ route, navigation }) {
   };
 
   return (
-    <View>
-      <Button
-        onPress={() => {
-          dispatch(addEvent({ form }));
-          navigation.goBack();
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: colors.primary, padding: 24 }}
+    >
+      <View
+        style={{
+          flexGrow: 0,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: 12,
         }}
       >
-        Save
-      </Button>
-      <Text>Going to</Text>
-      <Text>spend</Text>
+        <IconButton
+          icon={"close"}
+          style={{ marginLeft: -6 }}
+          color={colors.primary2}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        ></IconButton>
+        <Button
+          labelStyle={{ color: colors.accent2 }}
+          onPress={() => {
+            dispatch(addEvent({ form }));
+            navigation.goBack();
+          }}
+          mode="contained"
+          style={{
+            backgroundColor: colors.primary2,
+            alignSelf: "center",
+          }}
+        >
+          Save
+        </Button>
+      </View>
+
+      <Text
+        style={{
+          fontSize: 36,
+          color: colors.accent,
+          marginBottom: 12,
+        }}
+      >
+        Going to
+      </Text>
+      <Text
+        style={{
+          fontSize: 36,
+          color: colors.accent,
+          marginBottom: 12,
+        }}
+      >
+        spend
+      </Text>
       <TextInput
+        style={{ marginBottom: 12, marginHorizontal: 24 }}
         label="Amount"
         onChangeText={handleAmount}
         value={form.amount}
       ></TextInput>
-      <Text>on</Text>
+      <Text
+        style={{
+          fontSize: 36,
+          color: colors.accent,
+          marginBottom: 12,
+        }}
+      >
+        on
+      </Text>
       <TextInput
+        style={{ marginBottom: 12, marginHorizontal: 24 }}
         name={"name"}
         label="Name"
         onChangeText={handleName}
         value={form.name}
       ></TextInput>
-      <Button
+      <TouchableRipple
         icon={"arrow-down"}
+        color={colors.primary2}
+        style={{ marginBottom: 12 }}
         onPress={() => navigation.navigate("CreateCategory")}
       >
-        {form.category || "category"}
-      </Button>
-      <Text>Every</Text>
+        <Text
+          style={{ padding: 24, fontSize: 20, color: "#ffffff", flexGrow: 0 }}
+        >
+          {form.category || "Category"}
+        </Text>
+      </TouchableRipple>
+      <Text
+        style={{
+          fontSize: 36,
+          color: colors.accent,
+          marginBottom: 12,
+        }}
+      >
+        every
+      </Text>
       <Menu
         visible={visible}
         onDismiss={handleVisible}
         anchor={
-          <Button onPress={handleVisible}>{days[form.day] || "Day"}</Button>
+          <TouchableRipple
+            style={{ padding: 24 }}
+            color={"#ffffff"}
+            onPress={handleVisible}
+          >
+            <Text style={{ fontSize: 20, color: "#ffffff" }}>
+              {days[form.day]}
+            </Text>
+          </TouchableRipple>
         }
       >
         {days.map((el, index) => (
-          <Menu.Item title={el} key={el} onPress={() => handleDay(index)} />
+          <Menu.Item
+            style={{ textTransform: "capitalize" }}
+            title={el}
+            key={el}
+            onPress={() => handleDay(index)}
+          />
         ))}
       </Menu>
-    </View>
+      <KeyboardAvoidingView
+        style={{
+          width: "60%",
+          height: "30%",
+          position: "absolute",
+          bottom: 0,
+          right: -2,
+        }}
+        keyboardVerticalOffset={-500}
+      >
+        <Image
+          style={{
+            width: "100%",
+            height: "100%",
+            resizeMode: "contain",
+          }}
+          source={require("../assets/pig.png")}
+        />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
+
+StyleSheet.create({});
