ffa70e423327a00b7b4394a022daf3a97410a3aa
haoshan98 <haoshanw@gmail.com>
2020-12-28 14:50:00 +0800

    create modal

--- src/screens/profile-screen.js
+++ src/screens/profile-screen.js
@@ -1,6 +1,8 @@
-import React from "react";
-import {StyleSheet, View, Image, KeyboardAvoidingView, Dimensions, ScrollView} from "react-native";
+import React, {useState, useEffect} from "react";
+import {StyleSheet, View, Image, KeyboardAvoidingView, Dimensions, ScrollView,
+        TouchableHighlight, Alert} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
+import { Icon } from 'react-native-elements';
 import {
   TextInput,
   Text,
@@ -10,15 +12,13 @@ import {
   IconButton,
   TouchableRipple,
   FAB,
-  ProgressBar,
   DataTable,
   Card,
-  Title,
-  Paragraph
+  Paragraph,
+  Modal, Portal, Provider
 } from "react-native-paper";
 import {
   BarChart,
-  ProgressChart,
 } from "react-native-chart-kit";
 import {useDispatch} from "react-redux";
 import {createUUID} from "../utils/index";
@@ -33,6 +33,8 @@ const userInfo = {
 
 const data = {
   labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  // labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  //          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
   datasets: [
     {
       data: [20, 45, 28, 80, 99, 43]
@@ -57,8 +59,10 @@ const expenses = [
 
 export default function ProfileScreen({route, navigation}) {
   const {colors} = useTheme();
+
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
+
   const chartConfig = {
     backgroundGradientFrom: colors.primary3,
     backgroundGradientFromOpacity: 0,
@@ -66,47 +70,29 @@ export default function ProfileScreen({route, navigation}) {
     backgroundGradientToOpacity: 0,
     color: (opacity = 1) => `rgba(244, 128, 36, ${opacity})`,
     // color: (opacity = 1) => `rgba(240, 207, 163, ${opacity})`,
-    // labelcolor: (opacity = 1) => `rgba(26, 26, 26, ${opacity})`,
     strokeWidth: 5, // optional, default 3
     barPercentage: 0.8,
-    useShadowColorFromDataset: false // optional
   };
-  const [form, setForm] = React.useState({
-    uid: createUUID(),
-    amount: null,
-    category: null,
-    name: null,
-    day: new Date().getDay(),
-  });
-
-  React.useEffect(() => {
-    if (route.params?.category) {
-      setForm({...form, category: route.params.category});
-    }
-  }, [route.params?.category]);
 
   const [visible, setVisible] = React.useState(false);
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+  const [goal, setGoal] = useState(null);
+
+  useEffect(() => {
+  }, []);
+
   const dispatch = useDispatch();
+
 
   return (
     <SafeAreaView
       style={{flex: 1, backgroundColor: colors.primary2, padding: 24}}
     >
       <ScrollView style={styles.scrollView}>
-        <View
-          style={{
-            flexGrow: 0,
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 36,
-              color: colors.primary3,
-            }}
-          >
+        <View  style={viewTop()}>
+          <Text style={text1(colors.primary3)}>
             Hi
           </Text>
           <IconButton
@@ -119,41 +105,20 @@ export default function ProfileScreen({route, navigation}) {
             }}
           ></IconButton>
         </View>
-        <Text
-          style={{
-            fontSize: 36,
-            color: colors.primary3,
-            marginBottom: 10,
-            fontWeight: "bold"
-          }}
-        >
+        <Text style={text2(colors.primary3)}>
           {userInfo.name}
         </Text>
-        <Text
-          style={{
-            fontSize: 18,
-            color: colors.primary3,
-            marginBottom: 10,
-          }}
-        >
+        <Text style={text3(colors.primary3)}>
           Take a look at your accomplishment
         </Text>
 
         {/*--------streak & score---------------------------------*/}
 
-        <View
-          style={{
-            display: "flex",
-            flexGrow: 0,
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
+        <View style={twoCardView()}>
           <Card
             style={{
               flex: 1,
-              borderRadius: 10,
+              borderRadius: 5,
               backgroundColor: colors.primary,
               marginRight: 5
             }}>
@@ -166,22 +131,28 @@ export default function ProfileScreen({route, navigation}) {
                   alignItems: "center",
                 }}
               >
-                <Text
-                  style={{
-                    fontSize: 30,
-                    color: colors.primary3,
-                  }}
-                >
+                <Text style={{fontSize: 30, color: colors.primary3,}}>
                   {`${userInfo.streak}`}
                 </Text>
-                <Text
+                <View
                   style={{
-                    fontSize: 15,
-                    color: colors.primary3,
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                    alignItems: "center",
                   }}
                 >
-                  Hot Streak
-                </Text>
+                  <View style={{marginRight:5}}>
+                    <Text style={{fontSize: 15, color: colors.primary3,}}>
+                      Hot
+                    </Text>
+                    <Text style={{fontSize: 15, color: colors.primary3,}}>
+                      Streak
+                    </Text>
+                  </View>
+                  <Icon name='fire' type='font-awesome' color={"yellow"}/>
+                  {/*<Icon name='FireTwoTone' type='antdesign' color={"yellow"}/>*/}
+                </View>
               </View>
 
             </Card.Content>
@@ -189,7 +160,7 @@ export default function ProfileScreen({route, navigation}) {
           <Card
             style={{
               flex: 1,
-              borderRadius: 10,
+              borderRadius: 5,
               backgroundColor: colors.secondary,
               marginLeft: 5
             }}>
@@ -202,20 +173,10 @@ export default function ProfileScreen({route, navigation}) {
                   alignItems: "center",
                 }}
               >
-                <Text
-                  style={{
-                    fontSize: 30,
-                    color: colors.primary3,
-                  }}
-                >
+                <Text style={{fontSize: 30, color: colors.primary3,}}>
                   {`${userInfo.score}%`}
                 </Text>
-                <Text
-                  style={{
-                    fontSize: 20,
-                    color: colors.primary3,
-                  }}
-                >
+                <Text style={{fontSize: 20, color: colors.primary3,}}>
                   Score
                 </Text>
               </View>
@@ -227,7 +188,7 @@ export default function ProfileScreen({route, navigation}) {
 
         <Card
           style={{
-            borderRadius: 10,
+            borderRadius: 5,
             backgroundColor: colors.primary3,
             marginTop: 10,
             marginBottom: 10
@@ -241,11 +202,7 @@ export default function ProfileScreen({route, navigation}) {
                 alignItems: "center",
               }}
             >
-              <View
-                style={{
-                  flex: 1,
-                  marginRight: 10
-                }}>
+              <View style={{flex: 1, marginRight: 10}}>
                 <Text
                   style={{
                     fontSize: 18,
@@ -258,10 +215,7 @@ export default function ProfileScreen({route, navigation}) {
                 </Text>
               </View>
 
-              <View
-                style={{
-                  flex: 1.5,
-                }}>
+              <View style={{flex: 1.5,}}>
                 <View
                   style={{
                     display: "flex",
@@ -270,43 +224,10 @@ export default function ProfileScreen({route, navigation}) {
                     alignItems: "center",
                   }}
                 >
-                  <View
-                    style={{
-                      width: 180*(userInfo.goalProgress/100),
-                      height: 25,
-                      marginTop: 10,
-                      backgroundColor: colors.secondary,
-                      borderRadius: 20
-                  }}></View>
-                  <View
-                    style={{
-                      width: 180*((115-userInfo.goalProgress)/100),
-                      height: 25,
-                      marginLeft: -20,
-                      marginTop: 10,
-                      backgroundColor: colors.primary,
-                      borderRadius: 20,
-                      zIndex: -1
-                  }}></View>
+                  <View style={progressBarLeft(userInfo, colors.secondary)}></View>
+                  <View style={progressBarRight(userInfo, colors.primary)}></View>
                 </View>
-                {/*<ProgressBar*/}
-                {/*  progress={userInfo.goalProgress / 100}*/}
-                {/*  color={colors.secondary}*/}
-                {/*  style={{*/}
-                {/*    height: 15,*/}
-                {/*    marginTop: 10,*/}
-                {/*    color: colors.secondary,*/}
-                {/*    backgrondColor: colors.primary,*/}
-                {/*    borderRadius: 20*/}
-                {/*  }}*/}
-                {/*/>*/}
-                <Text
-                  style={{
-                    fontSize: 13,
-                    color: colors.text,
-                    marginTop: 10
-                  }}
-                >
+                <Text style={{fontSize: 13, color: colors.text, marginTop: 10}}>
                   {`You have completed ${userInfo.goalProgress}%. Keep it up!`}
                 </Text>
               </View>
@@ -315,15 +236,13 @@ export default function ProfileScreen({route, navigation}) {
           </Card.Content>
         </Card>
 
-
         {/*-----chart------------------------------------*/}
-
         <View>
           <BarChart
             style={{
               paddingTop: 15,
               paddingBottom: 10,
-              borderRadius: 10,
+              borderRadius: 5,
               backgroundColor: colors.primary3,
               // backgroundColor: 'rgba(10,10,10,0.7)',
             }}
@@ -334,24 +253,17 @@ export default function ProfileScreen({route, navigation}) {
             withVerticalLabels
             withHorizontalLabels
             showValuesOnTopOfBars
-            fromZero
+            fromZero={false}
             withInnerLines={false}
             chartConfig={chartConfig}
-            verticalLabelRotation={0}
-            horizontalLabelRotation={0}
+            // verticalLabelRotation={0}
+            // horizontalLabelRotation={0}
           />
         </View>
 
         {/*-----expenses table------------------------------------*/}
-
         <View
-          style={{
-            marginTop: 10,
-            marginBottom: 20,
-            padding: 10,
-            borderRadius: 10,
-            backgroundColor: colors.primary3,
-          }}>
+          style={tableView(colors.primary3)}>
           <DataTable>
             <DataTable.Pagination
               page={1}
@@ -359,13 +271,9 @@ export default function ProfileScreen({route, navigation}) {
               onPageChange={page => {
                 console.log(page);
               }}
-              label={<Text
-                style={{
+              label={<Text style={{
                   fontSize: 20, fontWeight: "bold", color: colors.text,
-                }}
-              >
-                {"Today"}
-              </Text>}
+                }}>{"Today"}</Text>}
             />
 
             <DataTable.Header>
@@ -388,76 +296,213 @@ export default function ProfileScreen({route, navigation}) {
                 </Text>
               </DataTable.Cell>
             </DataTable.Header>
-
             {expenses.map((expense) => (
               <DataTable.Row>
                 <DataTable.Cell>{expense.item}</DataTable.Cell>
                 <DataTable.Cell numeric>{expense.amount}</DataTable.Cell>
               </DataTable.Row>
             ))}
-
           </DataTable>
 
         </View>
-        <View
-          style={{
-            marginLeft: 8,
-            marginBottom: 3,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 12,
-              color: colors.text,
-            }}
-          >
+        <View style={{marginLeft: 8, marginBottom: 3}}>
+          <Text style={{fontSize: 12, color: colors.text,}}>
             Version 0.1
           </Text>
-          <Text
-            style={{
-              fontSize: 12,
-              color: colors.text,
-            }}
-          >
+          <Text style={{fontSize: 12, color: colors.text,}}>
             Created by Hello Cannot
           </Text>
         </View>
 
       </ScrollView>
 
+      <Portal>
+        <Modal visible={visible} onDismiss={hideModal}
+               contentContainerStyle={{backgroundColor: 'white',
+                 paddingTop: 20, paddingBottom: 20,
+                 marginLeft: 50, marginRight:50,
+                 borderRadius:5}}
+        >
+          <View style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'}}>
+            <View>
+              <Text
+                style={{
+                  fontSize: 22,
+                  fontWeight: "bold",
+                  color: colors.primary,
+                  marginBottom: 12,
+                }}
+              >
+                {goal? ("Edit Goal"):("Set Goal")}
+              </Text>
+              <TextInput
+                style={{ marginBottom: 12, marginHorizontal: 0, color:colors.accent2 }}
+                placeholderTextColor={colors.accent2}
+                underlineColorAndroid={colors.accent2}
+                label="Goal"
+                // onChangeText={handleAmount}
+                // value={form.amount}
+              ></TextInput>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "flex-end",
+                  alignItems: "center",
+                  marginTop: 10
+                }}
+              >
+                <TouchableHighlight>
+                  <Text style={{fontSize: 15, color: colors.accent2, marginRight:20, fontWeight: "bold",}}
+                        onPress={()=>hideModal()}>
+                    CANCEL
+                  </Text>
+                </TouchableHighlight>
+                <TouchableHighlight>
+                  <Text style={{fontSize: 15, color: colors.accent2, fontWeight: "bold",}}
+                        onPress={()=>console.log()}>
+                    CONFIRM
+                  </Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </Portal>
       <FAB icon="plus"
-           style={{
-             position: 'absolute',
-             margin: 16,
-             right: 0,
-             bottom: 0,
-             backgroundColor: colors.accent2
-           }}
-           onPress={() => console.log()}></FAB>
-
-      <View style={{
-        position: 'absolute',
-        marginLeft: windowWidth / 1.3,
-        marginTop: windowHeight / 6,
-        width: 280,
-        height: 280,
-        borderRadius: 300 / 2,
-        backgroundColor: colors.primary,
-        zIndex: -10
-      }}/>
-      <View style={{
-        position: 'absolute',
-        marginLeft: -50,
-        marginTop: windowHeight / 2,
-        width: 300,
-        height: 300,
-        borderRadius: 300 / 2,
-        backgroundColor: colors.secondary,
-        zIndex: -10
-      }}/>
-
+           style={fab(colors.accent2)}
+           onPress={() => showModal()}>
+      </FAB>
+      <View style={backgroundCircle1(windowWidth, windowHeight,colors.primary)} />
+      <View style={backgroundCircle2(windowHeight,colors.secondary)} />
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
+
+const viewTop = function() {
+  return {
+    flexGrow: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
+}
+
+const text1 = function(primary3) {
+  return {
+    fontSize: 36,
+    color: primary3,
+  }
+}
+
+const text2 = function(primary3) {
+  return {
+    fontSize: 36,
+    color: primary3,
+    marginBottom: 10,
+    fontWeight: "bold"
+  }
+}
+
+const text3 = function(primary3) {
+  return {
+    fontSize: 18,
+    color: primary3,
+    marginBottom: 10,
+  }
+}
+
+const twoCardView = function() {
+  return {
+    display: "flex",
+    flexGrow: 0,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
+}
+
+const cardInnerSplit = function() {
+  return {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  }
+}
+
+const progressBarLeft = function(userInfo, secondary) {
+  return {
+    width: 180*(userInfo.goalProgress/100),
+    height: 25,
+    marginTop: 10,
+    backgroundColor: secondary,
+    borderRadius: 20
+  }
+}
+
+const progressBarRight = function(userInfo, primary) {
+  return {
+    width: 180*((115-userInfo.goalProgress)/100),
+    height: 25,
+    marginLeft: -20,
+    marginTop: 10,
+    backgroundColor: primary,
+    borderRadius: 20,
+    zIndex: -1
+  }
+}
+
+const tableView = function(primary3) {
+  return {
+    marginTop: 10,
+    marginBottom: 25,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: primary3,
+  }
+}
+
+const fab = function(accent2) {
+  return {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+    backgroundColor: accent2
+  }
+}
+
+const backgroundCircle1 = function(windowWidth, windowHeight, primary) {
+  return {
+    position: 'absolute',
+    marginLeft: windowWidth / 1.3,
+    marginTop: windowHeight / 6,
+    width: 280,
+    height: 280,
+    borderRadius: 300 / 2,
+    backgroundColor: primary,
+    zIndex: -10
+  }
+}
+
+const backgroundCircle2 = function(windowHeight, secondary) {
+  return {
+    position: 'absolute',
+    marginLeft: -50,
+    marginTop: windowHeight / 2,
+    width: 300,
+    height: 300,
+    borderRadius: 300 / 2,
+    backgroundColor: secondary,
+    zIndex: -10
+  }
+}
