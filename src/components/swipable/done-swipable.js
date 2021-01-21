a17b4fca64370c55b05db29d53ba52a005ce29ce
Lee Yee Run <leerun1409@gmail.com>
2021-01-21 12:28:02 +0800

    Refactoring swipable (#15)
    * draft a new folder
    
    * remove unused styles
    
    * fix isOverspent boolean
    
    * style little for done-swipable

---
+++ src/components/swipable/done-swipable.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { useTheme } from "react-native-paper";
+import Swipeable from "react-native-swipeable";
+import { useDispatch } from "react-redux";
+
+export default ({ callback, item }) => {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+  const color = item.isOverspent
+    ? theme.colors.primary
+    : theme.colors.secondary;
+
+  const handleActionRelease = () => {
+    // TODO add toggle uncomplete
+
+    if (callback) {
+      callback();
+    }
+  };
+
+  return (
+    <Swipeable
+      onLeftActionRelease={handleActionRelease}
+      leftContent={
+        <View
+          style={[
+            styles.swipeItem,
+            styles.leftSwipeItem,
+            { backgroundColor: color },
+          ]}
+        >
+          <Image
+            source={require("../../assets/clear.png")}
+            style={{ width: 24 }}
+          ></Image>
+        </View>
+      }
+    >
+      <Text
+        style={[
+          styles.completedText,
+          {
+            color,
+          },
+        ]}
+      >
+        {`     ${item.name} for ${item.amount}`}{" "}
+      </Text>
+    </Swipeable>
+  );
+};
+
+const styles = StyleSheet.create({
+  swipeItem: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  leftSwipeItem: {
+    alignItems: "flex-end",
+    paddingRight: 12,
+  },
+  completedText: {
+    fontSize: 20,
+    marginHorizontal: 10,
+    padding: 10,
+    textDecorationLine: "line-through",
+  },
+});
