6193a577f97ada40418188dd1203d6467ebadc95
lee1409 <leerun1409@gmail.com>
2021-01-23 13:15:31 +0800

    reformat and remove auth

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,11 @@
 // In App.js in a new project
 
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  useNavigation,
+  useNavigationState,
+} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./home-screen";
 import EventScreen from "./event-screen";
@@ -13,6 +17,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Constants from "expo-constants";
 import * as Notifications from "expo-notifications";
 import * as Permissions from "expo-permissions";
+import { AppState } from "react-native";
 const Stack = createStackNavigator();
 
 Notifications.setNotificationHandler({
@@ -28,16 +33,14 @@ export default function App() {
   const notificationListener = React.useRef();
   const responseListener = React.useRef();
   //redux - store keys
-  const {allowAuth, firstAccess} = useSelector((state) => state.keys);
+  const { allow_auth: allowAuth, first_access: firstAccess } = useSelector(
+    (state) => state.keys
+  );
   const dispatch = useDispatch();
   React.useEffect(() => {
+    // Dealing with notification
     if (firstAccess) {
       schedulePushNotification();
-      if (!allowAuth) {
-        dispatch(updtDisableAuth());
-      } else {
-        dispatch(updtEnableAuth());
-      }
     }
     registerForPushNotificationsAsync().then((token) =>
       setExpoPushToken(token)
@@ -52,11 +55,13 @@ export default function App() {
         console.log(response);
       }
     );
+
     return () => {
       Notifications.removeNotificationSubscription(notificationListener);
       Notifications.removeNotificationSubscription(responseListener);
     };
   }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator headerMode="none">
